6805aba1bab27cfc780657eea36dd37065a791b7
wannabecoder7 <shikhar4090@gmail.com>
2025-07-24 17:58:36 +0530

    added css

--- headlines.py
+++ headlines.py
@@ -1,7 +1,9 @@
 import feedparser
-from flask import Flask, render_template
-from flask import request
-
+from flask import Flask, render_template, request, make_response
+import json
+import urllib.request
+import urllib.parse
+import datetime
 
 app = Flask(__name__)
 
@@ -9,23 +11,65 @@ RSS_FEEDS = {
     'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
-    'iol': 'http://www.iol.co.za/cmlink/1.640',
-    'ndtv': 'https://www.ndtv.com'
+    'iol': 'http://www.iol.co.za/cmlink/1.640'
 }
 
-@app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-    query = request.args.get("publication")
+DEFAULTS = {
+    'publication': 'bbc',
+    'city': 'London,UK'
+}
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
-    
     feed = feedparser.parse(RSS_FEEDS[publication])
-    entries = feed.entries
-    return render_template("index.html", articles=feed['entries'])
+    return feed['entries']
 
+def get_weather(query):
+    api_key = "f329d49ba89e71ada7320b73521b0f2f"  # Replace with your real key
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format("{}", api_key)
+
+    query_encoded = urllib.parse.quote(query)
+    url = api_url.format(query_encoded)
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+        parsed = json.loads(data)
+    except urllib.error.HTTPError as e:
+        print("Weather fetch failed:", e)
+        return None
+
+    if parsed.get("weather"):
+        return {
+            "description": parsed["weather"][0]["description"],
+            "temperature": parsed["main"]["temp"],
+            "city": parsed["name"],
+            "country": parsed["sys"]["country"]
+        }
+    return None
+
+@app.route("/")
+def home():
+    # get customized headlines
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+
+    # get customized weather
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
+    response = make_response(render_template("index.html", articles=articles, weather=weather))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    return response
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
